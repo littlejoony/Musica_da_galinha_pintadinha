608565eddf29d273d2fddce7045c0ac4aa554a46
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-03 21:14:41 -0300

    primeira tristeza programacional

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Dó"); Console.Beep(1320, 200);
+Console.WriteLine("Ré"); Console.Beep(1485, 200 );
+Console.WriteLine("Mi"); Console.Beep(1650, 200);
+Console.WriteLine("Fá"); Console.Beep(1759, 200);
+
+    Thread.Sleep(600);
+
+Console.WriteLine("Fá"); Console.Beep(1759, 200);
+Console.WriteLine("Fá");Console.Beep(1759, 200);
+
+    Thread.Sleep(1200);
+
+
+Console.WriteLine("Dó"); Console.Beep(1320, 200);
+Console.WriteLine("Ré"); Console.Beep(1485, 200 );
+Console.WriteLine("Dó"); Console.Beep(1320, 200);
+Console.WriteLine("Ré"); Console.Beep(1485, 200);
+
+    Thread.Sleep(600);
+
+Console.WriteLine("Ré"); Console.Beep(1485, 200);
+Console.WriteLine("Ré"); Console.Beep(1485, 200);
+
+
+    Thread.Sleep(1200);
+
+
+Console.WriteLine("Dó"); Console.Beep(1320, 200);
+Console.WriteLine("Sol"); Console.Beep(1980, 200);
+Console.WriteLine("Fá"); Console.Beep(1759,200);
+Console.WriteLine("Mi"); Console.Beep(1650,200);
+
+    Thread.Sleep(600);
+
+Console.WriteLine("Mi"); Console.Beep(1650,200);
+Console.WriteLine("Mi"); Console.Beep(1650,200);
+
+    Thread.Sleep(1200);
+
+Console.WriteLine("Dó"); Console.Beep(1320, 200);
+Console.WriteLine("Ré"); Console.Beep(1485, 200);
+Console.WriteLine("Mi"); Console.Beep(1650,200);
+Console.WriteLine("Fá"); Console.Beep(1759,200);
+
+    Thread.Sleep(600);
+
+Console.WriteLine("Fá"); Console.Beep(1759,200);
+Console.WriteLine ("Fá"); Console.Beep(1759,200);
+
+
+
